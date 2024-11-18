95932f82a25025c1299d0c2a91f17f5edf1eae24
bereed <18423688+bereed@users.noreply.github.com>
2024-11-17 21:44:51 -0600

    Create sw.js

---
+++ viewer/sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'panaderia-cache-v1';
+const assets = [
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
